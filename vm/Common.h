android/platform_dalvik

Pad the 8 and 16 bit Jvalue members
This allows them to be consistently accessed as either 8-bit,
16-bit or 32-bit values in bigendian and little-endian mode.

Change-Id: I1be5519f3666a3553b404368075f5a7b5d9e6b32
Signed-off-by: Chris Dearman <chris@mips.com>
Signed-off-by: Raghu Gandham <raghu@mips.com>

vm/Common.h
@@ -102,6 +102,7 @@ typedef int64_t             s8;
 struct Object;
 
 union JValue {
+# if defined(HAVE_LITTLE_ENDIAN)
     u1      z;
     s1      b;
     u2      c;
@@ -111,6 +112,30 @@ union JValue {
     float   f;
     double  d;
     Object* l;
+#endif
+# if defined(HAVE_BIG_ENDIAN)
+    struct {
+	u1	_z[3];
+	u1      z;
+    };
+    struct {
+	s1	_b[3];
+	s1      b;
+    };
+    struct {
+	u2	_c;
+	u2      c;
+    };
+    struct {
+	s2	_s;
+	s2      s;
+    };
+    s4      i;
+    s8      j;
+    float   f;
+    double  d;
+    void*   l;
+#endif
 };
 
 #define OFFSETOF_MEMBER(t, f)         \

Evervolv/android_dalvik

vm/Common.h
@@ -102,6 +102,7 @@ typedef int64_t             s8;
 struct Object;
 
 union JValue {
+#if defined(HAVE_LITTLE_ENDIAN)
     u1      z;
     s1      b;
     u2      c;
@@ -111,6 +112,30 @@ union JValue {
     float   f;
     double  d;
     Object* l;
+#endif
+#if defined(HAVE_BIG_ENDIAN)
+    struct {
+        u1    _z[3];
+        u1    z;
+    };
+    struct {
+        s1    _b[3];
+        s1    b;
+    };
+    struct {
+        u2    _c;
+        u2    c;
+    };
+    struct {
+        s2    _s;
+        s2    s;
+    };
+    s4      i;
+    s8      j;
+    float   f;
+    double  d;
+    void*   l;
+#endif
 };
 
 #define OFFSETOF_MEMBER(t, f)         \